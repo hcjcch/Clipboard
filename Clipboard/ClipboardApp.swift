cc1177fd4f2ee00ae851d52b28cc72c98a2edcfb
huangchen.102 <huangchen.102@jd.com>
2025-12-30 19:43:31 +0800

    Initial Commit

---
+++ Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by huangchen.102 on 2025/12/30.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClipboardTests/ClipboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardTests.swift
+//  ClipboardTests
+//
+//  Created by huangchen.102 on 2025/12/30.
+//
+
+import Testing
+@testable import Clipboard
+
+struct ClipboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
